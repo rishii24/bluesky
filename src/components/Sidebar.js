6d8f2386cb36b8341192daf908598478fb06404e
rishii24 <24rishi02@gmail.com>
2022-09-12 21:40:47 +0530

    auth sidebar created

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='md:flex hidden h-screen w-1/3 bg-primary justify-center items-center'>
+      <div className='text-white text-5xl font-bold'>Dash.</div>
+    </div>
   )
 }
 
