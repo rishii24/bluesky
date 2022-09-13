61f8e76bbe9cc9d821632c15e7ef3e9053f1a125
rishii24 <24rishi02@gmail.com>
2022-09-13 19:57:36 +0530

    navbar added

--- src/features/Settings.js
+++ src/features/Settings.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Header from '../components/Header';
 import Navbar from "../components/Navbar";
 
 
 const Settings = () => {
   return (
-    <div className="flex">
-      <Navbar />
-      <div>Settings</div>
+    <div className="flex bg-background">
+    <Navbar />
+    <div className="flex flex-col w-4/5 px-10 py-12">
+      <Header title="Settings"/>
+      <div></div>
     </div>
+  </div>
   )
 }
 
