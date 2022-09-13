463e3bf6cff6e080d8120e388aa668b7ef9e954d
rishii24 <24rishi02@gmail.com>
2022-09-13 19:57:00 +0530

    info card fpor dashboard

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const InfoCard = () => {
+const InfoCard = ({ title, value, CompImg, bgColor }) => {
   return (
-    <div>InfoCard</div>
-  )
-}
+    <div
+      className="flex flex-col w-44 px-4 py-3 rounded-xl"
+      style={{ background: `${bgColor}` }}
+    >
+      <div className="flex w-full justify-end"><img src={CompImg} alt="img"/></div>
+      <div className="mt-2">
+        <div className="text-xs font-semibold">{title}</div>
+        <div className="text-xl font-bold">{value}</div>
+      </div>
+    </div>
+  );
+};
 
-export default InfoCard
+export default InfoCard;
