77224c698fcf1f69cb850bf4b1d8a3f64e498d25
rishii24 <24rishi02@gmail.com>
2022-09-13 19:57:15 +0530

    userinfo page done

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const UserInfo = () => {
+const UserInfo = ({ userimg, firstName, lastName, email, index }) => {
   return (
-    <div>UserInfo</div>
-  )
-}
+    <div
+      className="flex justify-between items-center w-full px-4 py-2 mt-4 rounded-xl text-sm font-medium"
+      style={{ backgroundColor: index % 2 ? "#EDEDED" : "#F5F5F5" }}
+    >
+      <div>
+        <img src={userimg} alt="img" className="h-8 rounded-full" />
+      </div>
+      <div className="flex justify-start w-1/5">
+        {firstName} {lastName}
+      </div>
+      <div className="flex justify-start w-1/5">{email}</div>
+      <div className="flex justify-start w-16">
+        {index % 2 ? "Male" : "Female"}
+      </div>
+      <div className="flex justify-start w-16">
+        {index == 0 ? "Admin " : "User"}
+      </div>
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
