72ff761b0d999cbb4c3afcfabbfd58954480060a
rishii24 <24rishi02@gmail.com>
2022-09-14 16:24:12 +0530

    pie cahrt with dummy data

--- src/components/charts/PieChart.js
+++ src/components/charts/PieChart.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
 
-const PieChart = () => {
+const PieChartComp = () => {
+  const data = [
+    { name: "Group A", value: 100 },
+    { name: "Group B", value: 500 },
+    { name: "Group C", value: 200 },
+  ];
+
+  const colors = ["#EE8484", "#98D89E", "#F6DC7D"];
+
   return (
-    <div>PieChart</div>
-  )
-}
+    <PieChart width={150} height={150}>
+      <Pie data={data} cx="50%" cy="50%" outerRadius={60}>
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index]} />
+        ))}
+      </Pie>
+    </PieChart>
+  );
+};
 
-export default PieChart
+export default PieChartComp;
